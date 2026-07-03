d279b09e7644d378ad656cce8ad94099697f5892
SOL-OLAYINKA <thelobbyinside@gmail.com>
2026-07-03 14:52:30 -0400

    Update

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**.supabase.co' }],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.daregamesapp.com' }],
+        destination: 'https://daregamesapp.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
